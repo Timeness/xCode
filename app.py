7af9e34a6225467784f86cafe615b606580fee8b
Timeness <careless@flash.co>
2025-01-09 13:54:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -351,5 +351,51 @@ async def aexec(code, app, msg):
     return await locals()["__aexec"](app, msg)
 
 
+import io
+import sys
+import traceback
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.types import Message
+
+async def aexec(code, app, msg):
+    exec(
+        "async def aexec(app, msg): "
+        + "\n p = print"
+        + "\n repy= msg.reply_to_message"
+        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+    )
+    return await locals()"__aexec"
+
+@app.on_message(filters.command("p2"))
+@app.on_edited_message(filters.command("p2"))
+async def runPyro_Funcs(app: app, msg: Message) -> None:
+    code = msg.text.split(None, 1)
+    if len(code) == 1:
+        return await msg.reply(" ᴄᴏᴅᴇ ɴᴏᴛ ғᴏᴜɴᴅ !")
+    message = await msg.reply("ʀᴜɴɴɪɴɢ...")
+    soac = datetime.now()
+    osder = sys.stderr
+    osdor = sys.stdout
+    redr_opu = sys.stdout = io.StringIO()
+    redr_err = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        vacue = await aexec(code[1], app, msg)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redr_opu.getvalue()
+    stderr = redr_err.getvalue()
+    sys.stdout = osdor
+    sys.stderr = osder
+    evason = exc or stderr or stdout or vacue or "ɴᴏ ᴏᴜᴛᴘᴜᴛ"
+    eoac = datetime.now()
+    runcs = (eoac - soac).microseconds / 1000
+    oucode = f"📎 ᴄᴏᴅᴇ:\n{code[1]}\n📒 ᴏᴜᴛᴘᴜᴛ:\n{evason}\n✨ ᴛɪᴍᴇ ᴛᴀᴋᴇɴ: {runcs}ᴍɪʟɪsᴇᴄᴏɴᴅ"
+    if len(oucode) > 69696969:
+        await message.edit("⚠️ ᴏᴜᴛᴘᴜᴛ ᴛᴏᴏ ʟᴏɴɢ...")
+    else:
+        await message.edit(oucode)
+
 app.run()
 app.send_message(-1002361092154, "__**Chutiya Started**__")
